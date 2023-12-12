6947b62fa02d9542e5f3f597e4ac0fbc5990e29a
sim <sim@advte.com>
2023-12-11 20:51:06 -0500

    Implement stack and queue

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,47 @@
+
+type Node<T> = {
+   value: T,
+   prev?: Node<T>,
+}
+
 export default class Stack<T> {
-    public length: number;
 
-    
+   public length: number;
+   private head?: Node<T>;
 
-    constructor() {
-    }
+   constructor() {
+      this.head = undefined;
+      this.length = 0;
+   }
 
-    push(item: T): void {
+   push(item: T): void {
+      const node = { value: item } as Node<T>;
+      this.length++;
+      node.prev = this.head;
+      this.head = node;
+   }
 
-}
-    pop(): T | undefined {
+   pop(): T | undefined {
+      if (!this.head) {
+         return undefined;
+      }
 
-}
-    peek(): T | undefined {
+      this.length--;
+      
+      const head = this.head;
+      this.head = this.head.prev;
 
-}
+      if (this.length === 0) {
+         this.head = undefined;
+      }
+
+      return head.value;
+   }
+
+   peek(): T | undefined {
+      if (!this.head) {
+         return undefined;
+      }
+      return this.head.value;
+   }
 }
